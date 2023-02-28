87086518b1359e11caa00fa01853456ba15dd963
darlan <darlan.silvadacruz@gmail.com>
2023-02-28 11:19:32 -0300

    first commit

---
+++ server/src/main/java/com/project/dungeonfinder/DungeonfinderApplication.java
@@ -0,0 +1,13 @@
+package com.project.dungeonfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonfinderApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/project/dungeonfinder/DungeonfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dungeonfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

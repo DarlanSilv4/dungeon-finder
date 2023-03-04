7dda47d5fbc1edf9f22f599c3e043d2c49109f3b
darlan <darlan.silvadacruz@gmail.com>
2023-03-03 21:42:09 -0300

    (UserController) implement GET method to fetch all users

--- server/src/main/java/com/project/dungeonfinder/user/UserController.java
+++ server/src/main/java/com/project/dungeonfinder/user/UserController.java
@@ -1,10 +1,12 @@
 package com.project.dungeonfinder.user;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import java.util.Map;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +38,10 @@ public class UserController {
     return ResponseEntity
       .status(HttpStatus.CREATED)
       .body(userService.save(user));
+  }
+
+  @GetMapping
+  public ResponseEntity<List<User>> getListOfUsers() {
+    return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
   }
 }

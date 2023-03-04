812584b259e0eede658a8c654ea24ba5f93e0039
darlan <darlan.silvadacruz@gmail.com>
2023-03-04 11:14:48 -0300

    (UserController) implement DELETE method

--- server/src/main/java/com/project/dungeonfinder/user/UserController.java
+++ server/src/main/java/com/project/dungeonfinder/user/UserController.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,5 +60,22 @@ public class UserController {
     }
 
     return ResponseEntity.status(HttpStatus.OK).body(userOptional.get());
+  }
+
+  @DeleteMapping(path = "/{id}")
+  public ResponseEntity<Object> deleteUser(
+    @PathVariable(value = "id") UUID id
+  ) {
+    Optional<User> userOptional = userService.findById(id);
+    if (!userOptional.isPresent()) {
+      return ResponseEntity
+        .status(HttpStatus.NOT_FOUND)
+        .body(Map.entry("message", "user not found!"));
+    }
+
+    userService.delete(userOptional.get());
+    return ResponseEntity
+      .status(HttpStatus.OK)
+      .body(Map.entry("message", "user deleted successfully!"));
   }
 }
